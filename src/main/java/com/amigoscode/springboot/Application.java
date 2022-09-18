d4fba906e7514a2a4f28dfcc3491b9ce46b98e18
Mersudin Hasanovic <2170007646@iau.edu.sa>
2022-09-18 20:53:38 +0200

    Spring Boot - Initial upload of the Spring Initializr files

---
+++ src/main/java/com/amigoscode/springboot/Application.java
@@ -0,0 +1,23 @@
+package com.amigoscode.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+/**
+ * First of all, we are going to add annotation @RestController
+ */
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	public String hell0() {
+		return "Hello from @RestController";
+	}
+
+}
